2d3298ab391295289961b7a7105bd90142e12cc6
SouthBegonia <1687634916@qq.com>
2019-10-02 20:07:30 +0800

    添加 快排/堆排

---
+++ Algorithm/Sort/HeapSort.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace HeapSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+            HeapSort(numbers);
+
+            foreach (int i in numbers)
+                Console.Write(i + " ");
+
+            Console.ReadKey();
+        }
+
+        //堆排序函数：
+        public static void HeapSort(int[] arr)
+        {
+            if (arr == null || arr.Length < 2)
+                return;
+
+            //大根堆的实现
+            for (int i = 0; i < arr.Length; ++i)
+                HeapInsert(arr, i);
+
+            //将堆顶(最大值)交换到堆底，堆大小减小
+            int heapSize = arr.Length;
+            Swap(arr, 0, --heapSize);
+
+            //进行堆调整
+            while (heapSize > 0)
+            {
+                Heapify(arr, 0, heapSize);
+                Swap(arr, 0, --heapSize);
+            }
+        }
+
+        //堆插入函数（向上
+        public static void HeapInsert(int[] arr,int index)
+        {
+            //如果当前节点大于其父节点，则交换
+            while (arr[index] > arr[(index - 1) / 2])
+            {
+                Swap(arr, index, (index - 1) / 2);
+                index = (index - 1) / 2;
+            }
+        }
+
+        //堆调整函数（向下
+        public static void Heapify(int[] arr, int index,int size)
+        {
+            //扫描左孩子
+            int left = index * 2 + 1;
+            while (left < size)
+            {
+                //取得左右孩子中较大的下标
+                int largest = left + 1 < size && arr[left + 1] > arr[left] ? left + 1 : left;
+
+                largest = arr[largest] > arr[index] ? largest : index;
+
+                if (largest == index)
+                    break;
+
+                Swap(arr, largest, index);
+                index = largest;
+                left = index * 2 + 1;
+            }
+        }
+
+        public static void Swap(int[] arr, int A, int B)
+        {
+            int temp = arr[A];
+            arr[A] = arr[B];
+            arr[B] = temp;
+        }
+    }
+}
---
+++ Algorithm/Sort/QuickSort.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 9,8,7,6,5,4,3,2,1,0 };
+
+            QuickSort(arr, 0, 9);
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadKey();
+        }
+
+        public static void QuickSort(int[] arr, int L,int R)
+        {
+            if (L < R)
+            {
+                //随机选择一个划分值
+                Random random = new Random();
+                Swap(arr, L + random.Next(R - L), R);
+
+                int[] p = Partition(arr, L, R);
+                QuickSort(arr, L, p[0] - 1);
+                QuickSort(arr, p[1] + 1, R);
+            }
+        }
+
+        public static int[] Partition(int[] arr, int L, int R)
+        {
+            int less = L - 1;
+            int more = R;
+
+            while (L < more)
+            {
+                if (arr[L] < arr[R])
+                    Swap(arr, ++less, L++);
+                else if (arr[L] > arr[R])
+                    Swap(arr, --more, L);
+                else
+                    L++;
+            }
+            Swap(arr, more, R);
+            return new int[] { less + 1, more };
+        }
+
+        public static void Swap(int[] arr, int A,int B)
+        {
+            int temp = arr[A];
+            arr[A] = arr[B];
+            arr[B] = temp;
+        }
+    }
+}
