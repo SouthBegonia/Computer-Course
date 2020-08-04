2c38f676f0084a158d274fe0e4307401cb03a0d0
SouthBegonia <1687634916@qq.com>
2020-08-04 23:06:25 +0800

    添加 IEnumerable练习

---
+++ Practice/Csharp/20200804_IEnumerable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Timers;
+
+namespace CsharpExam
+{
+    class _20200804_IEnumerable
+    {
+        private static void Main(string[] args)
+        {
+            foreach (int i in Test())
+            {
+                Console.WriteLine("返回的结果是：" + i);
+            }
+        }
+
+        public static IEnumerable<int> Test()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                yield return i;
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
