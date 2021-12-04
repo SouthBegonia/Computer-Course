31841ac589dc30e4199d2d9ba4081debbc1422e4
SouthBegonia <southbegonia@foxmail.com>
2021-12-04 22:35:58 +0800

    添加 直接插入排序 练习

---
+++ Practice/Csharp/20211204_直接插入排序.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CsharpExam
+{
+    class _20211204_InsertSort
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 9,8,7,6,5,4,3,2,1,0 };
+
+            
+            //直接插入排序
+            InSertSort(ref arr);
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadKey();
+        }
+
+        #region 直接插入排序
+        public static void InSertSort(ref int[] arr)
+        {
+            int len = arr.Length;
+            int curNum;
+            int preIndex;
+            
+            for (int i = 1; i < len; i++)
+            {
+                curNum = arr[i];
+                preIndex = i - 1;
+
+                while (preIndex >= 0 && curNum < arr[preIndex])
+                {
+                    arr[preIndex + 1] = arr[preIndex];
+                    --preIndex;
+                }
+                arr[preIndex + 1] = curNum;
+            }
+        }
+        #endregion
+    }
+}
