ebaac9f0d69ef52afdc91b56b7c5a01b2517a4b6
SouthBegonia <southbegonia@foxmail.com>
2021-12-05 16:09:48 +0800

    添加 连续子数组最大和 练习

---
+++ Practice/Csharp/20211205_连续子数组最大和.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CsharpExam
+{
+    class _20211205_MaxAddOfArray
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1,-2,3,10,-4,7,2,-5 };
+
+            Console.Write(GetMaxAddOfArray(arr));
+
+            Console.ReadKey();
+        }
+
+        #region 连续子数组的最大和
+        static int GetMaxAddOfArray(int[] arr)
+        {
+            if (arr == null || arr.Length < 1)
+                return 0;
+
+            int len = arr.Length;
+            int maxAdd = arr[0];
+            int curTotal = arr[0];
+
+            for (int i = 1; i < len; i++)
+            {
+                if (curTotal < 0)
+                    curTotal = arr[i];
+                else
+                    curTotal += arr[i];
+
+                if (curTotal > maxAdd)
+                    maxAdd = curTotal;
+            }
+
+            return maxAdd;
+        }
+        #endregion
+    }
+}
