40dadbfa8eae2ced927cdd62e41d8494368a82bf
SouthBegonia <southbegonia@foxmail.com>
2022-01-03 20:56:52 +0800

    添加 练习
    爬楼梯
    有效的括号

---
+++ Practice/Csharp/20220103_爬楼梯.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace CsharpExam
+{
+    public class _20220103_爬楼梯
+    {
+        public int ClimbStairs(int n)
+        {
+            if (n <= 1)
+                return 1;
+
+            int[] result = new int[n + 1];
+            result[1] = 1;
+            result[2] = 2;
+
+            for (int i = 3; i <= n; i++)
+            {
+                result[i] = result[i - 1] + result[i - 2];
+            }
+
+            return result[n];
+        }
+    }
+}
