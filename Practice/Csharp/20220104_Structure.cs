6886fc5e6ca6bdd80807043a56160dea90917a32
SouthBegonia <southbegonia@foxmail.com>
2022-01-04 18:21:38 +0800

    添加 结构体参数传递的坑 练习

---
+++ Practice/Csharp/20220104_Structure.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace CsharpExam
+{
+    public class _20220104_Structure
+    {
+        static void Main(string[] args)
+        {
+            Transform t = new Transform();
+
+            //Compiler Error CS1612  https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/compiler-messages/cs1612
+            t.v.x = 1;
+
+            //Fix
+            Vector tempV = t.v;
+            tempV.x = 1;
+            t.v = tempV;
+
+            t.ShowV();
+            Console.Read();
+        }
+
+
+        public struct Vector
+        {
+            public float x;
+            public float y;
+            public float z;
+        }
+
+        public class Transform
+        {
+            Transform()
+            {
+                Vector newV = new Vector();
+                newV.x = 1;
+                newV.y = 2;
+                newV.z = 3;
+
+                v = newV;
+            }
+
+            public Vector v { get; set; }
+
+            public void ShowV()
+            {
+                Console.WriteLine(v.x + "..." + v.y + "..." + v.z);
+            }
+        }
+    }
+}
