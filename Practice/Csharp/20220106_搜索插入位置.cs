8e69662490f7711e74490c62f0f97150ba1149ed
SouthBegonia <southbegonia@foxmail.com>
2022-01-06 22:15:42 +0800

    添加 搜索插入位置 练习

---
+++ Practice/Csharp/20220106_搜索插入位置.cs
@@ -0,0 +1,27 @@
+﻿namespace CsharpExam
+{
+    //https://leetcode-cn.com/leetbook/read/array-and-string/cxqdh/
+    public class _20220106_搜索插入位置
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+            int middle;
+
+            while (left <= right)
+            {
+                middle = left + (right - left) / 2;
+
+                if (nums[middle] == target)
+                    return middle;
+                else if (nums[middle] < target)
+                    left = middle + 1;
+                else if (nums[middle] > target)
+                    right = middle - 1;
+            }
+
+            return left;
+        }
+    }
+}
