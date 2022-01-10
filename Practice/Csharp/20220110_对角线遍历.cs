f00abb0d981fa7be58b13429eb04d6cf30d44b98
SouthBegonia <southbegonia@foxmail.com>
2022-01-10 21:13:13 +0800

    添加 对角线遍历/零矩阵 练习

---
+++ Practice/Csharp/20220110_对角线遍历.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CsharpExam
+{
+    public class _20220110_对角线遍历
+    {
+        static void Main()
+        {
+            int[][] matrix3 = new int[3][];
+            matrix3[0] = new[] {1, 2, 3, 0};
+            matrix3[1] = new[] {4, 5, 6, 0};
+            matrix3[2] = new[] {7, 8, 9, 0};
+
+            _20220110_对角线遍历 test = new _20220110_对角线遍历();
+            var result = test.FindDiagonalOrder(matrix3);
+        }
+
+        public int[] FindDiagonalOrder(int[][] mat)
+        {
+            int rows = mat?.Length - 1 ?? 0;
+            int cols = rows >= 0 ? mat[0].Length - 1 : 0;
+
+            int x = 0;
+            int y = 0;
+
+            List<int> result = new List<int>(mat?.Length ?? 0);
+
+            for (int i = 0; i <= rows + cols; i++)
+            {
+                if (i % 2 != 0)
+                {
+
+                    //奇数层：左下到右上
+                    for (int j = y; j >= i - x; j--)
+                        result.Add(mat[i - j][j]);
+                }
+                else
+                {
+                    //偶数层：右上到左下
+                    for (int j = x; j >= i - y; j--)
+                        result.Add(mat[j][i - j]);
+                }
+
+                x = x >= rows? rows : x + 1;
+                y = y >= cols? cols : y + 1;
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ Practice/Csharp/20220110_零矩阵.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace CsharpExam
+{
+    //https://leetcode-cn.com/leetbook/read/array-and-string/ciekh/
+    public class _20220110_零矩阵
+    {
+        public void SetZeroes(int[][] matrix)
+        {
+            int rows = matrix?.Length ?? 0;
+            int cols = rows > 0 ? matrix[0].Length : 0;
+
+            //待删除行列的index
+            List<int> rowDelIndexList = new List<int>();
+            List<int> colDelIndexList = new List<int>();
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (matrix[i][j] == 0)
+                    {
+                        if (!rowDelIndexList.Contains(i))
+                            rowDelIndexList.Add(i);
+
+                        if (!colDelIndexList.Contains(j))
+                            colDelIndexList.Add(j);
+                    }
+                }
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (rowDelIndexList.Contains(i) || colDelIndexList.Contains(j))
+                        matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
