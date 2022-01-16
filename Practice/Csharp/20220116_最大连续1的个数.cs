180edae16c658c62c0e5a770d92829fcecdde938
SouthBegonia <southbegonia@foxmail.com>
2022-01-16 11:23:08 +0800

    添加 双指针 练习

---
+++ Practice/Csharp/20220116_最大连续1的个数.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CsharpExam
+{
+    //https://leetcode-cn.com/leetbook/read/array-and-string/cd71t/
+    public class _20220116_最大连续1的个数
+    {
+        public int FindMaxConsecutiveOnes(int[] nums)
+        {
+            int len = nums.Length;
+            int ans = 0;
+            int zeroIndex = -1;
+
+            for (int i = 0; i < len; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    zeroIndex = i;
+
+                }
+                else
+                {
+                    //[1,1,0,1,1,1]
+                    ans = Math.Max(ans, i - zeroIndex);
+                }
+            }
+
+            return ans;
+        }
+    }
+}
---
+++ Practice/Csharp/20220116_长度最小的子数组.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CsharpExam
+{
+    //https://leetcode-cn.com/leetbook/read/array-and-string/c0w4r/
+    public class _20220116_长度最小的子数组
+    {
+        // static void Main()
+        // {
+        //     _20220116_长度最小的子数组 test = new _20220116_长度最小的子数组();
+        //
+        //     int[] arr = {2, 3, 1, 2, 4, 3};
+        //     int target = 7;
+        //     Console.WriteLine(test.MinSubArrayLen(target,arr));
+        // }
+
+        public int MinSubArrayLen(int target, int[] nums)
+        {
+            //滑动窗口
+            int minLen = nums.Length + 1;
+            int left = 0;
+            int sum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                while (sum >= target)
+                {
+                    minLen = Math.Min(minLen, i - left + 1);
+                    sum -= nums[left];
+                    left++;
+                }
+            }
+
+            return minLen <= nums.Length ? minLen : 0;
+        }
+    }
+}
