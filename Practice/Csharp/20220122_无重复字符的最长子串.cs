452492d9daeccae63b14fd9b3da76ee1afb8ce91
SouthBegonia <southbegonia@foxmail.com>
2022-01-22 22:52:14 +0800

    添加 无重复字符的最长子串 练习

---
+++ Practice/Csharp/20220122_无重复字符的最长子串.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CsharpExam
+{
+    //https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/submissions/
+    public class _20220122_无重复字符的最长子串
+    {
+        public int LengthOfLongestSubstring2(string s)
+        {
+            //暴力遍历
+            int minLen = 0;
+            int len = s.Length;
+            List<char> list = new List<char>(len);
+
+            for (int i = 0; i < len; i++)
+            {
+                int nextIndex = i;
+                list.Clear();
+
+                //每次都以i为起点向后遍历
+                while (nextIndex < len)
+                {
+                    if (list.Contains(s[nextIndex]))
+                        break;
+                    else
+                    {
+                        list.Add(s[nextIndex]);
+                        minLen = Math.Max(minLen, list.Count);
+                        ++nextIndex;
+                    }
+                }
+            }
+            return minLen;
+        }
+    }
+}
