ff4f4e9f9504358f5ff495dd5d4f60f2675d4204
SouthBegonia <southbegonia@foxmail.com>
2022-01-25 18:28:20 +0800

    添加 C# 练习

---
+++ Practice/Csharp/20220125_ClassTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CsharpExam
+{
+    public class _20220125_ClassTest
+    {
+        // static void Main()
+        // {
+        //     Student st = new Student(18, "Joker");
+        // }
+
+        abstract class Person
+        {
+            public string Name;
+
+            public Person(string Name)
+            {
+                this.Name = Name;
+                Console.WriteLine($"Init Person.  Name={Name}");
+            }
+        }
+
+        sealed class Student : Person
+        {
+            public int Age;
+
+            public Student(int Age, string Name) : base(Name)
+            {
+                this.Age = Age;
+                Console.WriteLine($"Init Student.  Name={Name}, Age={Age}");
+            }
+        }
+    }
+}
