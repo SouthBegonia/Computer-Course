4119b2c1c0cea6b250fd3b71dd2640add6229387
SouthBegonia <southbegonia@foxmail.com>
2022-01-28 19:38:33 +0800

    添加 双栈实现队列/包含min函数的栈 练习

---
+++ Practice/Csharp/Offer_20220128_09.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace CsharpExam
+{
+    //https://leetcode-cn.com/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof/
+    // 09 两个栈实现队列
+    public class Offer_20220128_09
+    {
+        public class CQueue
+        {
+            private Stack<int> mainStack;
+            private Stack<int> tempStack;
+
+            public CQueue()
+            {
+                mainStack = new Stack<int>();
+                tempStack = new Stack<int>();
+            }
+
+            public void AppendTail(int value)
+            {
+                mainStack.Push(value);
+            }
+
+            public int DeleteHead()
+            {
+                if (tempStack.Count == 0)
+                {
+                    while (mainStack.Count>0)
+                        tempStack.Push(mainStack.Pop());
+                }
+
+                if (tempStack.Count == 0)
+                    return -1;
+                else
+                    return tempStack.Pop();
+
+                //低效：
+                // while (mainStack.Count > 0)
+                //     tempStack.Push(mainStack.Pop());
+                //
+                // int val = -1;
+                // if (tempStack.Count > 0)
+                //     val = tempStack.Pop();
+                //
+                // while (tempStack.Count > 0)
+                //     mainStack.Push(tempStack.Pop());
+                //
+                // return val;
+            }
+        }
+    }
+}
---
+++ Practice/Csharp/Offer_20220128_30.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CsharpExam
+{
+    //https://leetcode-cn.com/problems/bao-han-minhan-shu-de-zhan-lcof/
+    // 30 包含min函数的栈
+    public class Offer_20220128_30
+    {
+        public class MinStack
+        {
+            private Stack<int> stack;
+            private Stack<int> minStack;    //存储较小值的栈（非严格降序）
+            public MinStack()
+            {
+                stack = new Stack<int>();
+                minStack = new Stack<int>();
+            }
+
+            public void Push(int x)
+            {
+                stack.Push(x);
+                if (minStack.Count == 0 || minStack.Peek() >= x)    //只有较小之才入minStack栈
+                    minStack.Push(x);
+            }
+
+            public void Pop()
+            {
+                int val = stack.Pop();
+                if (minStack.Count > 0 && minStack.Peek() == val)
+                    minStack.Pop();
+            }
+
+            public int Top()
+            {
+                return stack.Count > 0 ? stack.Peek() : -1;
+            }
+
+            public int Min()
+            {
+                return minStack.Count > 0 ? minStack.Peek() : -1;
+            }
+        }
+    }
+}
