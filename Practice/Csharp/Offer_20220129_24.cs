9ea602327928e430be2c905607f023b2a5c78399
SouthBegonia <southbegonia@foxmail.com>
2022-01-29 13:57:16 +0800

    添加 反转链表 练习

---
+++ Practice/Csharp/Offer_20220129_24.cs
@@ -0,0 +1,36 @@
+﻿namespace CsharpExam
+{
+    //https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof/
+    // 24 反转链表
+    public class Offer_20220129_24
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+
+            public ListNode(int x)
+            {
+                val = x;
+            }
+        }
+
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode preNode = null;
+            ListNode curNode = head;
+            ListNode nextNode = head?.next;
+
+            while (curNode != null)
+            {
+                curNode.next = preNode;
+
+                preNode = curNode;
+                curNode = nextNode;
+                nextNode = nextNode?.next;
+            }
+
+            return preNode;
+        }
+    }
+}
