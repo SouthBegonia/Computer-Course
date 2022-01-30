c6d18c27ae0719a0fa39d83c6f86fc024f94ccf1
SouthBegonia <southbegonia@foxmail.com>
2022-01-30 13:06:52 +0800

    添加 左旋转字符串 练习

---
+++ Practice/Csharp/Offer_20220130_58.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CsharpExam
+{
+    public class Offer_20220130_58
+    {
+        //https://leetcode-cn.com/problems/zuo-xuan-zhuan-zi-fu-chuan-lcof/
+        // 58 左旋转字符串
+        public string ReverseLeftWords(string s, int n)
+        {
+            int len = s.Length;
+            n = n % len;
+
+            string left = s.Substring(0, n);
+            string right = s.Substring(n, len - n);
+            left = ReverseStr(left);
+            right = ReverseStr(right);
+
+            return ReverseStr(left + right);
+        }
+
+        string ReverseStr(string str)
+        {
+            char[] arr = str.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+    }
+}
