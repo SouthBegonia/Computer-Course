21ae3af17fbfe63a2f07b09f14b56b4e1d9acf75
SouthBegonia <southbegonia@foxmail.com>
2022-01-30 13:06:38 +0800

    添加 替换空格 练习

---
+++ Practice/Csharp/Offer_20220130_05.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace CsharpExam
+{
+    //https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/
+    // 05 替换空格
+    public class Offer_20220130_05
+    {
+        public string ReplaceSpace(string s)
+        {
+            char[] arr = s.ToArray();
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == ' ')
+                    sb.Append("%20");
+                else
+                    sb.Append(arr[i]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
