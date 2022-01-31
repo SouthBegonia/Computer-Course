64cba35031d426cc5f080c5da7acb1be5251aa28
SouthBegonia <southbegonia@foxmail.com>
2022-01-31 13:10:26 +0800

    添加 0~n中缺失的数字 练习

---
+++ Practice/Csharp/Offer_20220131_53_2.cs
@@ -0,0 +1,22 @@
+﻿namespace CsharpExam
+{
+    //https://leetcode-cn.com/problems/que-shi-de-shu-zi-lcof/
+    // 53 0~n中缺失的数字
+    public class Offer_20220131_53_2
+    {
+        public int MissingNumber(int[] nums)
+        {
+            int i = 0, j = nums.Length - 1;
+            while (i <= j)
+            {
+                int m = (i + j) / 2;
+                if (nums[m] == m)
+                    i = m + 1;
+                else
+                    j = m - 1;
+            }
+
+            return i;
+        }
+    }
+}
