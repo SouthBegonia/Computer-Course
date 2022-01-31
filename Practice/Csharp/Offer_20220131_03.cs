9f278eaa9abfc20ae271b64f3ac8fd9f2594107e
SouthBegonia <southbegonia@foxmail.com>
2022-01-31 13:09:58 +0800

    添加 数组中重复的数字 练习

---
+++ Practice/Csharp/Offer_20220131_03.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace CsharpExam
+{
+    //https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/
+    // 03 数组中重复的数字
+    public class Offer_20220131_03
+    {
+        public int FindRepeatNumber(int[] nums)
+        {
+            HashSet<int> table = new HashSet<int>();
+            ;
+            foreach (int num in nums)
+            {
+                if (table.Contains(num))
+                    return num;
+                else
+                    table.Add(num);
+            }
+
+            return -1;
+        }
+    }
+}
