8656b61155a1ec0d50ca3908b3d6f05d16884e8b
SouthBegonia <southbegonia@foxmail.com>
2022-02-03 17:06:29 +0800

    添加 二维数字中的查找 练习

---
+++ Practice/Csharp/Offer_20220203_04.cs
@@ -0,0 +1,27 @@
+﻿namespace CsharpExam
+{
+    //https://leetcode-cn.com/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof/
+    // 04 二维数字中的查找
+    public class Offer_20220203_04
+    {
+        public bool FindNumberIn2DArray(int[][] matrix, int target)
+        {
+            if (matrix == null || matrix.Length == 0 || matrix[0].Length == 0)
+                return false;
+
+            int row = matrix.Length, col = matrix[0].Length;
+            int i = 0, j = col - 1;
+
+            while (i < row && j >= 0)
+            {
+                if (target > matrix[i][j])
+                    i++;
+                else if (target < matrix[i][j])
+                    j--;
+                else
+                    return true;
+            }
+            return false;
+        }
+    }
+}
