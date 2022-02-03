a2c980ad0627cd7cf7cad6715f824f9570acebb1
SouthBegonia <southbegonia@foxmail.com>
2022-02-03 17:06:42 +0800

    添加 旋转数组里的最小数字 练习

---
+++ Practice/Csharp/Offer_20220203_11.cs
@@ -0,0 +1,31 @@
+﻿namespace CsharpExam
+{
+    //https://leetcode-cn.com/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof/
+    // 11 旋转数组里的最小数字
+    public class Offer_20220203_11
+    {
+        public int MinArray(int[] numbers)
+        {
+            int left = 0, right = numbers.Length - 1;
+
+            while (left < right)
+            {
+                int middle = (left + right) / 2;
+                if (numbers[middle] > numbers[right])
+                {
+                    left = middle + 1;
+                }
+                else if (numbers[middle] < numbers[right])
+                {
+                    right = middle;
+                }
+                else
+                {
+                    right -= 1;
+                }
+            }
+
+            return numbers[left];
+        }
+    }
+}
