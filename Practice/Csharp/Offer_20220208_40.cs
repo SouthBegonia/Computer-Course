d1a90ea0facfa4ba8709eec6598ddd26c79c6db0
SouthBegonia <southbegonia@foxmail.com>
2022-02-08 22:13:21 +0800

    添加 TopK 练习

---
+++ Practice/Csharp/Offer_20220208_40.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CsharpExam
+{
+    // https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof/
+    // 40 最小的K个数
+    public class Offer_20220208_40
+    {
+        public int[] GetLeastNumbers(int[] arr, int k)
+        {
+            Array.Sort(arr, (a, b) => { return a.CompareTo(b); });
+
+            List<int> list = new List<int>(arr.Length);
+            for (int i = 0; i < k; i++)
+                list.Add(arr[i]);
+
+            return list.ToArray();
+        }
+    }
+}
