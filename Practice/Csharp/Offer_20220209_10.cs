d1c6bedfb48854324526bb7185622e04ada2970b
SouthBegonia <southbegonia@foxmail.com>
2022-02-09 23:11:51 +0800

    添加 斐波那契数列 练习

---
+++ Practice/Csharp/Offer_20220209_10.cs
@@ -0,0 +1,26 @@
+﻿namespace CsharpExam
+{
+    // https://leetcode-cn.com/problems/fei-bo-na-qi-shu-lie-lcof/
+    // 10-1 斐波那契数列
+    public class Offer_20220209_10
+    {
+        public int Fib(int n)
+        {
+            if (n <= 1)
+                return n;
+
+            int a = 0;
+            int b = 1;
+            int sum = 0;
+
+            for (int i = 2; i <= n; i++)
+            {
+                sum = (a + b) % 1000000007;
+                a = b;
+                b = sum;
+            }
+
+            return sum;
+        }
+    }
+}
