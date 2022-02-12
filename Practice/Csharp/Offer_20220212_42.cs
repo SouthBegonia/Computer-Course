54533700a80e759cf1f35f623708a8526a14ed76
SouthBegonia <southbegonia@foxmail.com>
2022-02-12 21:04:32 +0800

    添加 连续子数组的最大和 练习

---
+++ Practice/Csharp/Offer_20220212_42.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CsharpExam
+{
+    // https://leetcode-cn.com/problems/lian-xu-zi-shu-zu-de-zui-da-he-lcof/
+    // 42 连续子数组的最大和
+    public class Offer_20220212_42
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            int curSum = nums[0];
+            int maxSum = curSum;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (curSum <= 0)
+                    curSum = nums[i];
+                else
+                    curSum += nums[i];
+
+                maxSum = Math.Max(maxSum, curSum);
+            }
+
+            return maxSum;
+        }
+    }
+}
