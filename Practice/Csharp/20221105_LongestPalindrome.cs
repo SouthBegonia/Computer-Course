c52a4fa9f522b1a6ce7b3becd994e5b6d81fe810
ZhengWei Zhao <southbegonia@foxmail.com>
2022-11-15 15:37:49 +0800

    添加 最长回文子串 练习

---
+++ Practice/Csharp/20221105_LongestPalindrome.cs
@@ -0,0 +1,54 @@
+﻿namespace CsharpExam
+{
+    /// <summary>
+    /// 最长回文子串
+    /// </summary>
+    public class _20221105_LongestPalindrome
+    {
+        public string LongestPalindrome(string s)
+        {
+            if (s.Length == 1)
+                return s;
+
+            int maxLen = 0;
+            int maxStrStartIndex = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                //奇数情况
+                int maxLenOnUnevenNum = GetStrMaxLen(s, i, i);
+                if (maxLenOnUnevenNum > maxLen)
+                {
+                    maxLen = maxLenOnUnevenNum;
+                    maxStrStartIndex = i - (maxLenOnUnevenNum - 1) / 2;
+                }
+
+                //偶数情况
+                int maxLenOnEvenNum = GetStrMaxLen(s, i, i + 1);
+                if (maxLenOnEvenNum > maxLen)
+                {
+                    maxLen = maxLenOnEvenNum;
+                    maxStrStartIndex = i - (maxLenOnEvenNum / 2 - 1);
+                }
+            }
+
+            return s.Substring(maxStrStartIndex, maxLen);
+        }
+
+        private int GetStrMaxLen(string s, int left, int right)
+        {
+            while (left >= 0 && right < s.Length)
+            {
+                if (s[left] == s[right])
+                {
+                    --left;
+                    ++right;
+                }
+                else
+                    break;
+            }
+
+            return right - left - 1;
+        }
+    }
+}
