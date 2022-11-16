501903138a4ec6b5721f06b616291c9b4134091c
SouthBegonia <southbegonia@foxmail.com>
2022-11-16 21:59:00 +0800

    添加 最后一个单词的长度 练习

---
+++ Practice/Csharp/_20221116_LengthOfLastWord.cs
@@ -0,0 +1,34 @@
+﻿namespace CsharpExam
+{
+    /// <summary>
+    /// 58 最后一个单词的长度  https://leetcode.cn/problems/length-of-last-word/
+    /// </summary>
+    public class _20221116_LengthOfLastWord
+    {
+        public int LengthOfLastWord(string s)
+        {
+            int lengthOfLastWord = 0;
+            int latterIndex = s.Length - 1;
+
+            while (latterIndex >= 0)
+            {
+                if (s[latterIndex] == ' ')
+                    --latterIndex;
+                else
+                    break;
+            }
+
+            int preIndex = latterIndex;
+            while (preIndex >= 0)
+            {
+                if (s[preIndex] != ' ')
+                    ++lengthOfLastWord;
+                else
+                    break;
+                --preIndex;
+            }
+
+            return lengthOfLastWord;
+        }
+    }
+}
