38dc0acc9c89bba642f280712dafc5043f8fcb2a
SouthBegonia <southbegonia@foxmail.com>
2022-11-17 22:27:49 +0800

    添加 爬楼梯 练习 修改 命名规范

---
+++ Practice/Csharp/20221117_ClimbStairs.cs
@@ -0,0 +1,22 @@
+﻿namespace CsharpExam
+{
+    /// <summary>
+    /// 70 爬楼梯  https://leetcode.cn/problems/climbing-stairs/
+    /// </summary>
+    public class _20221117_ClimbStairs
+    {
+        public int ClimbStairs(int n)
+        {
+            int[] result = new int[n + 1];
+            result[0] = 1;
+            result[1] = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                result[i] = result[i - 1] + result[i - 2];
+            }
+
+            return result[n];
+        }
+    }
+}
