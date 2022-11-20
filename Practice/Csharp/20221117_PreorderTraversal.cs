d6f88806e96ff2569ff7cef7bdb7c8199f831e3a
SouthBegonia <southbegonia@foxmail.com>
2022-11-20 21:06:30 +0800

    添加 二叉树的前中后序遍历 练习

--- Practice/Csharp/20221117_PreorderTraversal.cs
+++ Practice/Csharp/20221117_PreorderTraversal.cs
@@ -3,9 +3,9 @@
 namespace CsharpExam
 {
     /// <summary>
-    /// 94 二叉树的中序遍历  https://leetcode.cn/problems/binary-tree-inorder-traversal/
+    /// 144 二叉树的前序遍历  https://leetcode.cn/problems/binary-tree-preorder-traversal/submissions/
     /// </summary>
-    public class _20221117_InorderTraversal
+    public class _20221117_PreorderTraversal
     {
         public class TreeNode
         {
@@ -21,50 +21,30 @@ namespace CsharpExam
             }
         }
 
-        /*public IList<int> InorderTraversal(TreeNode root)
-        {
-            List<int> result = new List<int>();
-            DFS(result, root);
-            return result;
-        }
-
-        private void DFS(IList<int> result, TreeNode node)
-        {
-            if (node == null)
-                return;
-
-            DFS(result, node.left);
-            result.Add(node.val);
-            DFS(result, node.right);
-        }*/
-
-        /*
-        public IList<int> InorderTraversal(TreeNode root)
+        /*public IList<int> PreorderTraversal(TreeNode root)
         {
             List<int> result = new List<int>();
             Stack<TreeNode> stack = new Stack<TreeNode>();
 
+            stack.Push(root);
             TreeNode node;
-            while (stack.Count > 0 || root != null)
+            while (stack.Count > 0)
             {
-                if (root != null)
+                node = stack.Pop();
+                if (node != null)
                 {
-                    stack.Push(root);
-                    root = root.left;
-                }
-                else
-                {
-                    node = stack.Pop();
                     result.Add(node.val);
-                    root = node.right;
+                    if (node.right != null)
+                        stack.Push(node.right);
+                    if (node.left != null)
+                        stack.Push(node.left);
                 }
             }
 
             return result;
-        }
-        */
+        }*/
 
-        public IList<int> InorderTraversal(TreeNode root)
+        public IList<int> PreorderTraversal(TreeNode root)
         {
             List<int> result = new List<int>();
             Stack<TreeNode> stack = new Stack<TreeNode>();
@@ -76,14 +56,13 @@ namespace CsharpExam
                 //根节点及左侧全部节点入栈
                 while (curNode != null)
                 {
+                    result.Add(curNode.val);
                     stack.Push(curNode);
                     curNode = curNode.left;
                 }
 
-                //此节点的左子树遍历完毕，弹出记录，切换到右节点
-                TreeNode temp = stack.Pop();
-                result.Add(temp.val);
-                curNode = temp.right;
+                //此节点的左子树遍历完毕，切换到右节点
+                curNode = stack.Pop().right;
             }
 
             return result;
---
+++ Practice/Csharp/20221120_PostorderTraversal.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace CsharpExam
+{
+    /// <summary>
+    /// 145 二叉树的后序遍历  https://leetcode.cn/problems/binary-tree-postorder-traversal/submissions/
+    /// </summary>
+    public class _20221120_PostorderTraversal
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public IList<int> PostorderTraversal(TreeNode root)
+        {
+            List<int> result = new List<int>();
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+
+            TreeNode curNode = root;
+
+            while (stack.Count > 0 || curNode != null)
+            {
+                //根节点及右侧全部节点入栈
+                while (curNode != null)
+                {
+                    result.Add(curNode.val);
+                    stack.Push(curNode);
+                    curNode = curNode.right;
+                }
+
+                //此节点的右边子树遍历完毕，切换到左节点
+                curNode = stack.Pop().left;
+            }
+
+            //结果由 右左根 反转为 根左右（后序遍历）
+            result.Reverse(0, result.Count);
+
+            return result;
+        }
+    }
+}
