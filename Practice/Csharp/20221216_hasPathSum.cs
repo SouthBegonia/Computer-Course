54331f8d3b37bc544efd1286716f6f5b44f03d7c
SouthBegonia <southbegonia@foxmail.com>
2022-12-16 22:03:20 +0800

    添加 二叉树中和为某一值的路径 练习 添加 二叉树的最大深度 练习

---
+++ Practice/Csharp/20221216_hasPathSum.cs
@@ -0,0 +1,52 @@
+﻿namespace CsharpExam
+{
+    /// <summary>
+    /// 二叉树中和为某一值的路径  https://www.nowcoder.com/practice/508378c0823c423baa723ce448cbfd0c?tpId=295&tqId=634&ru=/exam/oj&qru=/ta/format-top101/question-ranking&sourceUrl=%2Fexam%2Foj
+    /// </summary>
+    public class _20221216_hasPathSum
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(int x)
+            {
+                val = x;
+            }
+        }
+
+        public bool hasPathSum(TreeNode root, int sum)
+        {
+            if (root == null)
+                return false;
+
+            return DFS(root, sum);
+        }
+
+        public bool DFS(TreeNode node, int target)
+        {
+            if (node == null)
+                return false;
+
+            //扣除当前节点值的
+            target -= node.val;
+
+            //查验当前节点是否符合
+            if (target == 0)
+            {
+                //叶子节点 且 最终等于sum值，符合
+                if (node.left == null && node.right == null)
+                    return true;
+
+                //注意：节点值可能为负数，因此不可忽略直接忽略子树部分
+                //else
+                //    return false;
+            }
+
+            //否则回溯深度遍历此节点的左右分支
+            return DFS(node.left, target) || DFS(node.right, target);
+        }
+    }
+}
---
+++ Practice/Csharp/20221216_maxDepth.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace CsharpExam
+{
+    /// <summary>
+    /// 二叉树的最大深度  https://www.nowcoder.com/practice/8a2b2bf6c19b4f23a9bdb9b233eefa73?tpId=295&tqId=642&ru=/exam/oj&qru=/ta/format-top101/question-ranking&sourceUrl=%2Fexam%2Foj
+    /// </summary>
+    public class _20221216_maxDepth
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(int x)
+            {
+                val = x;
+            }
+        }
+
+        public int maxDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            //层序遍历
+            int ret = 0;
+            while (queue.Count > 0)
+            {
+                int count = queue.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    TreeNode node = queue.Dequeue();
+                    if (node.left != null)
+                        queue.Enqueue(node.left);
+                    if (node.right != null)
+                        queue.Enqueue(node.right);
+                }
+
+                //遍历完一层
+                ++ret;
+            }
+
+            return ret;
+        }
+    }
+}
